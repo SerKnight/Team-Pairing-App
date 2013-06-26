9a6eb5853cff61de9ca8790c8f68e60f3d9e175c
Christopher <ChrisKnight.mail@gmail.com>
2013-06-26 13:53:17 -0600

    pics and cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,35 @@
 Team.delete_all
 Team.create([
-        { name: 'IFTTT', description: "This is a team description, generall 1 sentence about the team's mantra", id: 1 },
-        { name: 'Agile', description: "This is a team description, generall 1 sentence about the team's mantra", id: 2 },
-        { name: 'TDD', description: "This is a team description, generall 1 sentence about the team's mantra", id: 3 },
-        { name: 'DD', description: "This is a team description, generall 1 sentence about the team's mantra", id: 4 },
-        { name: 'Bananarama', description: "This is a team description, generall 1 sentence about the team's mantra", id: 5 },
-        { name: 'Alcoholics', description: "This is a team description, generall 1 sentence about the team's mantra", id: 6 }
+        { name: 'Design', description: "The 'pretty' team - HTML, CSS, Javascript Gurus. We like beautiful things, and fluid user experiences.", id: 1 },
+        { name: 'Rails', description: "The 'smart' team - core backend peoples. We like skinny models, and no logic in our views.", id: 2 },
+        { name: 'Marketing', description: "The 'money' team - We sell all the things & find out the best way to get us all paid.", id: 3 },
+        { name: 'Customer Outreach', description: "The 'philosophy' team - What is our product being used for.. and how can we make it better.", id: 4 }
 ])
 
 
 Member.delete_all
 Member.create([
-        { name: 'Kareem Grant', email: "rehpotsirhc46@gmail.com", team_ids: 1 },
-        { name: 'Chris Knight', email: "member@mail.com", team_ids: 1 },
-        { name: 'Daniel Mee', email: "member2@mail.com", team_ids: 1 },
-        { name: 'Laura Steadman', email: "member3@mail.com", team_ids: 1 },
-        { name: 'Elaine Tai', email: "member4@mail.com", team_ids: 1 },
-        { name: 'Chelsea Komlo', email: "member5@mail.com", team_ids: 1 },
-        { name: 'Raph Weiner', email: "member6@mail.com", team_ids: 1 },
-        { name: 'Paul blackwell', email: "member7@mail.com", team_ids: 1 },
-        { name: 'Josh Mejia', email: "member8@mail.com", team_ids: 1 },
-        { name: 'Shane Rogers', email: "member9@mail.com", team_ids: 1 },
-        { name: 'Geoffrey Schorpkof', email: "member10@mail.com", team_ids: 1 },
-        { name: 'Jeff Casimir', email: "member11@mail.com", team_ids: 1 },
-        { name: 'Franklin Webber', email: "member12@mail.com", team_ids: 1 }
+        { name: 'Rick Astly', email: "rickrolled@mail.com", team_ids: 1 },
+        { name: 'Derek Torr', email: "manager@mail.com", team_ids: 2 },
+        { name: 'Cam Merra', email: "photos@mail.com", team_ids: 1 },
+        { name: 'Devin Chee', email: "sistine@mail.com", team_ids: 1 },
+        { name: 'Blair Ring', email: "music@mail.com", team_ids: 1 },
+        { name: 'Sampson Night', email: "suitcase@mail.com", team_ids: [2, 1] },
+        { name: 'Laurie Aught', email: "nobel@mail.com", team_ids: 2 },
+        { name: 'Eley Feighnt', email: "postgresql@mail.com", team_ids: [2, 1] },
+        { name: 'Chance Hellor', email: "government@mail.com", team_ids: 2 },
+        { name: 'Dan Deelions', email: "flowers@mail.com", team_ids: 2 },
+        { name: 'Abdul Chaudrhy', email: "abdul@mail.com", team_ids: 2 },
+        { name: 'Devin Foley', email: "mboileman@mail.com", team_ids: [2, 1] },
+        { name: 'Jeff Lee', email: "jlee@mail.com", team_ids: [2, 1] },
+        { name: 'Kevin Ebaugh', email: "keshaluvr@mail.com", team_ids: [2, 1] },
+        { name: 'Lady', email: "pooch@mail.com", team_ids: 2 },
+        { name: 'Sloane Sturzenegger', email: "thecodernator@mail.com", team_ids: 1 },
+        { name: 'Leor Stern', email: "lordleor@mail.com", team_ids: 1 },
+        { name: 'Linden Tibbets', email: "tibetbro@mail.com", team_ids: 1 },
+        { name: 'Nate Murray', email: "catluvr@mail.com", team_ids: 1 },
+        { name: 'Shiva Kilaru', email: "supreme_god@mail.com", team_ids: [2, 1] },
+        { name: 'Alexander Tibbets', email: "tidbits@mail.com", team_ids: 2 },
+        { name: 'Christopher Knight', email: "rehpotsirhc46@gmail.com", team_ids: 2 },
+        { name: 'Anthony Accomazzo', email: "A_mazzo@mail.com", team_ids: [2, 1] }
 ])
