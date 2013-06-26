be046e36663a9387ae51497fd99ae144a9fa5e16
Christopher <ChrisKnight.mail@gmail.com>
2013-06-26 16:12:18 -0600

    sendgrid emails working. they have my cc..

--- config/environment.rb
+++ config/environment.rb
@@ -2,15 +2,18 @@
 require File.expand_path('../application', __FILE__)
 
 PeoplePerson::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => 'smtp.gmail.com',
-    :port                 => 587,
-    :domain               => 'baci.lindsaar.net',
-    :user_name            => 'chrisknight.mail@gmail.com',
-    :password             => ENV["EMAIL_PASSWORD"],
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+  
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ‘gschool’,
+  :password => ENV["EMAIL_PASSWORD"]
+  :domain => "http://people-person.herokuapp.com",
+  :address => ‘smtp.sendgrid.net’,
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+
 end
 
 # Initialize the Rails application.
