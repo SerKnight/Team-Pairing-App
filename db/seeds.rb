d1e209bc000d15453ca48dd9a246e1fce0499b70
SerKnight <chrisknight.mail@gmail.com>
2015-01-06 02:17:44 -1000

    digital telepathy is kewl

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,53 @@
 Member.delete_all
 Member.create([
-        { name: 'Rick Astly', email: "rickrolled@mail.com" },
-        { name: 'Derek Torr', email: "manager@mail.com" },
-        { name: 'Cam Merra', email: "photos@mail.com" },
-        { name: 'Devin Chee', email: "sistine@mail.com" },
-        { name: 'Blair Ring', email: "music@mail.com" },
-        { name: 'Sampson Night', email: "suitcase@mail.com" },
-        { name: 'Laurie Aught', email: "nobel@mail.com" },
-        { name: 'Eley Feighnt', email: "postgresql@mail.com" },
-        { name: 'Chance Hellor', email: "government@mail.com" },
-        { name: 'Dan Deelions', email: "flowers@mail.com" },
-        { name: 'Abdul Chaudrhy', email: "abdul@mail.com" },
-        { name: 'Devin Foley', email: "mboileman@mail.com" },
-        { name: 'Jeff Lee', email: "jlee@mail.com" },
-        { name: 'Kevin Ebaugh', email: "keshaluvr@mail.com" },
-        { name: 'Lady', email: "pooch@mail.com" },
-        { name: 'Sloane Sturzenegger', email: "thecodernator@mail.com" },
-        { name: 'Leor Stern', email: "lordleor@mail.com" },
-        { name: 'Linden Tibbets', email: "tibetbro@mail.com"  },
-        { name: 'Nate Murray', email: "catluvr@mail.com"  },
-        { name: 'Shiva Kilaru', email: "supreme_god@mail.com" },
-        { name: 'Alexander Tibbets', email: "tidbits@mail.com" },
-        { name: 'Christopher Knight', email: "rehpotsirhc46@gmail.com" },
-        { name: 'Anthony Accomazzo', email: "anthony@ifttt.com" }
+        { name: 'Arnold Yoon', email: "HeyArnold@mail.com" },
+        { name: 'Dana Larson', email: "ilikebeach@mail.com" },
+        { name: 'Brad Soroka', email: "EastCoastDiego@mail.com" },
+        { name: 'Jessica Moon', email: "GamingVigilante@mail.com" },
+        { name: 'Rocky Roark', email: "NerdzorDoodler@mail.com" },
+        { name: 'Jamie Hamel-Smith', email: "nerd^3D@mail.com" },
+        { name: 'Dave Shepard', email: "BeardedDev@mail.com" },
+        { name: 'Jason Amunwa', email: "productNinja@mail.com" },
+        { name: 'Brian Bimschleger', email: "white-surf-boards@mail.com" },
+        { name: 'Brent Summers', email: "musicRacer@mail.com" },
+        { name: 'Cody Iddings', email: "NintendoHawaiian@mail.com" },
+        { name: 'Annie Sexton', email: "CoffeeTexan@mail.com" },
+        { name: 'Dan Birman', email: "buffPainter@mail.com" },
+        { name: 'Melanie Chandruang', email: "QuickbookNinja@mail.com" },
+        { name: 'Dani Dufrene', email: "ScooooDucks@mail.com" },
+        { name: 'Bethany Brown', email: "saltLakeCode@mail.com" },
+        { name: 'Allison Drake', email: "advloAccountHolder@mail.com" },
+        { name: 'Dustin Lamont', email: "cornDogger@mail.com"  },
+        { name: 'MK Cook', email: "beePizza@mail.com"  },
+        { name: 'Ben Thiefels', email: "VTproud@mail.com" },
+        { name: 'Eric Thomas', email: "electronJams@mail.com" },
+        { name: 'Susie Pollasky', email: "storytellerTime@gmail.com" },
+        { name: 'Jimmy Ebeling', email: "chikenEatr@mail.com" }
+        { name: 'Marc Caldwell', email: "OceanSushi@mail.com" }
+        { name: 'Bradley Kenyon', email: "desToDev@mail.com" }
+        { name: 'Chuck Longanecker', email: "CoolEverythingOperator@mail.com" }
+        { name: 'Orville Esoy', email: "Wanderluster@mail.com" }
+        { name: 'Maurice Rosemond', email: "GeorgianThinkr@mail.com" }
+
+        { name: 'DAN TRENKNER', email: "AdvloGuy@mail.com" }
+        { name: 'SHANNON LONGANECKER', email: "FarmerMarketr@mail.com" }
+        { name: 'SHAWN LESLIE', email: "sunglasser@mail.com" }
+        { name: 'JOHN BOTICA', email: "BeerBrewer@mail.com" }
+        { name: 'ARON SCHUHMANN', email: "shredderWithPurpose@mail.com" }
+        { name: 'NATE SONNENBERG', email: "DigitalGuy@mail.com" }
+        { name: 'LAURENCE MONTGOMERY', email: "WSP4LYFE@mail.com" }
+        { name: 'SAM ANDERSON', email: "PRtoTacos@mail.com" }
+        { name: 'DAVID NGUYEN', email: "DaServicer@mail.com" }
+        { name: 'BRIAN BURKETT', email: "twitterFriend@mail.com" }
+        { name: 'NATE SONNENBERG', email: "DigitalGuy@mail.com" }
+        { name: 'Sara', email: "Dog1@mail.com" }
+        { name: 'Paisely', email: "dog2@mail.com" }
+        { name: 'Kibah', email: "dog3@mail.com" }
+        { name: 'luna', email: "moonDoggie@mail.com" }
+])
+
+Team.delete_all
+Team.create([
+        { name: 'designers', description: "Our designers have a huge impact on all of our current and future products. No pigeonholes here - your influence will be felt at every step of the product lifecycle, from concept & design, development, QA and launch, refinement and even ongoing optimization." }
+        { name: 'developers', description: "As our Ruby on Rails Developer, you’ll be working with the seasoned team behind products like SlideDeck, Hello Bar and Flare, as we take our next big step - Filament. As a key member of our Products Division, you’ll be responsible for building systems and bringing app concepts to life for our growing userbase of thousands. Join us in our mission to make the web better!" }
 ])
