f0d99f60bef5f4e33301b52b15980c58df35dc91
SerKnight <chrisknight.mail@gmail.com>
2015-01-06 02:21:22 -1000

    DT is kewl & i forgot commas in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,32 +22,32 @@ Member.create([
         { name: 'Ben Thiefels', email: "VTproud@mail.com" },
         { name: 'Eric Thomas', email: "electronJams@mail.com" },
         { name: 'Susie Pollasky', email: "storytellerTime@gmail.com" },
-        { name: 'Jimmy Ebeling', email: "chikenEatr@mail.com" }
-        { name: 'Marc Caldwell', email: "OceanSushi@mail.com" }
-        { name: 'Bradley Kenyon', email: "desToDev@mail.com" }
-        { name: 'Chuck Longanecker', email: "CoolEverythingOperator@mail.com" }
-        { name: 'Orville Esoy', email: "Wanderluster@mail.com" }
-        { name: 'Maurice Rosemond', email: "GeorgianThinkr@mail.com" }
+        { name: 'Jimmy Ebeling', email: "chikenEatr@mail.com" },
+        { name: 'Marc Caldwell', email: "OceanSushi@mail.com" },
+        { name: 'Bradley Kenyon', email: "desToDev@mail.com" },
+        { name: 'Chuck Longanecker', email: "CoolEverythingOperator@mail.com" },
+        { name: 'Orville Esoy', email: "Wanderluster@mail.com" },
+        { name: 'Maurice Rosemond', email: "GeorgianThinkr@mail.com" },
 
-        { name: 'DAN TRENKNER', email: "AdvloGuy@mail.com" }
-        { name: 'SHANNON LONGANECKER', email: "FarmerMarketr@mail.com" }
-        { name: 'SHAWN LESLIE', email: "sunglasser@mail.com" }
-        { name: 'JOHN BOTICA', email: "BeerBrewer@mail.com" }
-        { name: 'ARON SCHUHMANN', email: "shredderWithPurpose@mail.com" }
-        { name: 'NATE SONNENBERG', email: "DigitalGuy@mail.com" }
-        { name: 'LAURENCE MONTGOMERY', email: "WSP4LYFE@mail.com" }
-        { name: 'SAM ANDERSON', email: "PRtoTacos@mail.com" }
-        { name: 'DAVID NGUYEN', email: "DaServicer@mail.com" }
-        { name: 'BRIAN BURKETT', email: "twitterFriend@mail.com" }
-        { name: 'NATE SONNENBERG', email: "DigitalGuy@mail.com" }
-        { name: 'Sara', email: "Dog1@mail.com" }
-        { name: 'Paisely', email: "dog2@mail.com" }
-        { name: 'Kibah', email: "dog3@mail.com" }
+        { name: 'DAN TRENKNER', email: "AdvloGuy@mail.com" },
+        { name: 'SHANNON LONGANECKER', email: "FarmerMarketr@mail.com" },
+        { name: 'SHAWN LESLIE', email: "sunglasser@mail.com" },
+        { name: 'JOHN BOTICA', email: "BeerBrewer@mail.com" },
+        { name: 'ARON SCHUHMANN', email: "shredderWithPurpose@mail.com" },
+        { name: 'NATE SONNENBERG', email: "DigitalGuy@mail.com" },
+        { name: 'LAURENCE MONTGOMERY', email: "WSP4LYFE@mail.com" },
+        { name: 'SAM ANDERSON', email: "PRtoTacos@mail.com" },
+        { name: 'DAVID NGUYEN', email: "DaServicer@mail.com" },
+        { name: 'BRIAN BURKETT', email: "twitterFriend@mail.com" },
+        { name: 'NATE SONNENBERG', email: "DigitalGuy@mail.com" },
+        { name: 'Sara', email: "Dog1@mail.com" },
+        { name: 'Paisely', email: "dog2@mail.com" },
+        { name: 'Kibah', email: "dog3@mail.com" },
         { name: 'luna', email: "moonDoggie@mail.com" }
 ])
 
 Team.delete_all
 Team.create([
-        { name: 'designers', description: "Our designers have a huge impact on all of our current and future products. No pigeonholes here - your influence will be felt at every step of the product lifecycle, from concept & design, development, QA and launch, refinement and even ongoing optimization." }
+        { name: 'designers', description: "Our designers have a huge impact on all of our current and future products. No pigeonholes here - your influence will be felt at every step of the product lifecycle, from concept & design, development, QA and launch, refinement and even ongoing optimization." },
         { name: 'developers', description: "As our Ruby on Rails Developer, you’ll be working with the seasoned team behind products like SlideDeck, Hello Bar and Flare, as we take our next big step - Filament. As a key member of our Products Division, you’ll be responsible for building systems and bringing app concepts to life for our growing userbase of thousands. Join us in our mission to make the web better!" }
 ])
